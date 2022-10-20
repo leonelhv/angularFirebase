b41f3cdd7f63c0edff39e850f3eba2304f132ccf
Leonel Huachaca Velasquez <leonel.huachacav@gmail.com>
2022-10-19 22:36:28 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['dashboard']);
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule),
+    ...canActivate(redirectLoggedInToHome),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+    ...canActivate(redirectUnauthorizedToLogin),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ngbytes-fireauth-b5544',
+    appId: '1:91508441378:web:ece01a9a8553db4a74a57b',
+    storageBucket: 'ngbytes-fireauth-b5544.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB0085jgIvhNNhW5zrRHdxOgZk-b59VruE',
+    authDomain: 'ngbytes-fireauth-b5544.firebaseapp.com',
+    messagingSenderId: '91508441378',
+  },
   production: false
 };
 
